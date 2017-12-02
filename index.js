d27e4a13c90aa37ddbb574bd454a59ad0c91184f
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2017-12-02 22:59:39 +0100

    feat: Add stop function

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ module.exports.autowire = (conf) => {
 
   map((n) => app.use(require(n.package)(...n.options)), conf.koa.middlewares)
 
-  var methodRoute = (method) => propEq('method', method)
-  var isGetRoute = methodRoute('get')
-  var isPostRoute = methodRoute('post')
-  var isPutRoute = methodRoute('put')
-  var isDeleteRoute = methodRoute('delete')
-  var getFilter = filter(isGetRoute, conf.endpoints)
-  var postFilter = filter(isPostRoute, conf.endpoints)
-  var putFilter = filter(isPutRoute, conf.endpoints)
-  var deleteFilter = filter(isDeleteRoute, conf.endpoints)
+  const methodRoute = (method) => propEq('method', method)
+  const isGetRoute = methodRoute('get')
+  const isPostRoute = methodRoute('post')
+  const isPutRoute = methodRoute('put')
+  const isDeleteRoute = methodRoute('delete')
+  const getFilter = filter(isGetRoute, conf.endpoints)
+  const postFilter = filter(isPostRoute, conf.endpoints)
+  const putFilter = filter(isPutRoute, conf.endpoints)
+  const deleteFilter = filter(isDeleteRoute, conf.endpoints)
 
   map((n) => router.get(...prepend(n.route, n.middlewares)), getFilter)
   map((n) => router.post(...prepend(n.route, n.middlewares)), postFilter)
@@ -29,6 +29,7 @@ module.exports.autowire = (conf) => {
 
   app.context.db = conf.db
   app.context.server = conf.server
+  app.context.router = router
 
   return app
 }
@@ -37,6 +38,14 @@ module.exports.start = (app) => {
   const http = require('http')
   const https = require('https')
 
-  http.createServer(app.callback()).listen(app.context.server.httpPort)
-  https.createServer(app.context.server, app.callback()).listen(app.context.server.httpsPort)
+  const httpServer = http.createServer(app.callback()).listen(app.context.server.httpPort)
+  const httpsServer = https.createServer(app.context.server, app.callback()).listen(app.context.server.httpsPort)
+
+  app.context.httpServer = httpServer
+  app.context.httpsServer = httpsServer
+}
+
+module.exports.stop = (app) => {
+  app.context.httpServer.close()
+  app.context.httpsServer.close()
 }
